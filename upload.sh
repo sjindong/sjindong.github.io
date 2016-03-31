3b03aa00782d2a139de7b3f122a2393f2501fb7e
sjindong <shengxdong@gmail.com>
2016-03-31 14:47:10 +0800

    脚本upload.sh 提交

---
+++ upload.sh
@@ -0,0 +1,13 @@
+cp -r /home/sjd/code/backup/public/. /home/sjd/code/sjindong.github.io/.
+git add 2016
+git add archives
+git add css
+git add fancybox
+git add font
+git add img
+git add js
+git add index.html
+git add atom.xml
+git add upload.sh
+git commit -m "脚本upload.sh 提交"
+git push origin master
